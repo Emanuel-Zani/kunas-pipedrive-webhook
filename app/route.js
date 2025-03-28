c8992d15327d4b59f64827f502f57c9ebe1cf923
Emanuel Zani <manuzani10@gmail.com>
2025-03-28 13:04:29 -0300

    phone

--- app/route.js
+++ app/route.js
@@ -116,7 +116,7 @@ async function addDeal(reservationDetails, personaId, niños,) {
     //Cantidad de niños
     '5f41eab7a51a40acbf99a24d8dc36a7f5786cf86': niños ?? "No especificado",
     //Teléfono
-    phone: reservationDetails.phone ?? "No especificado",
+    ac907fd34e67f90bab739453da5642cfc79dbf3a: reservationDetails.phone ?? "No especificado",
     //País
     '91643604b4916086cf51d676af68bcb53b7c4d44': reservationDetails.country ?? "No especificado",
 
